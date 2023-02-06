5a2421d868911470edccf9286abbb4aa88994fe2
孤之界 <f20001012@gmail.com>
2023-02-06 13:59:08 +0800

    add: 初始化 TwitCasting 通知

---
+++ Discord Stream Notify Bot/HttpClients/TwitCastingClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+
+namespace Discord_Stream_Notify_Bot.HttpClients
+{
+    public class TwitCastingClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly BotConfig _botConfig;
+
+        public TwitCastingClient(HttpClient httpClient, BotConfig botConfig)
+        {
+            _httpClient = httpClient;
+            _botConfig = botConfig;
+        }
+    }
+}
---
+++ Discord Stream Notify Bot/SharedService/TwitCasting/TwitCastingService.cs
@@ -0,0 +1,14 @@
+﻿using Discord_Stream_Notify_Bot.Interaction;
+
+namespace Discord_Stream_Notify_Bot.SharedService.TwitCasting
+{
+    public class TwitCastingService : IInteractionService
+    {
+        private readonly DiscordSocketClient _client;
+
+        public TwitCastingService(DiscordSocketClient client)
+        {
+            _client = client;
+        }
+    }
+}
