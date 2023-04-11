4e309852692ce00b1870e320cdf37957222e90a1
孤之界 <f20001012@gmail.com>
2023-04-11 14:46:01 +0800

    add: 排除 TaskCanceledException 錯誤紀錄

--- Discord Stream Notify Bot/Log.cs
+++ Discord Stream Notify Bot/Log.cs
@@ -121,7 +121,7 @@ public static class Log
         WriteLogToFile(LogType.Verb, message.Message);
 #endif
 
-        if (message.Exception != null && message.Exception is not Discord.WebSocket.GatewayReconnectException)
+        if (message.Exception != null && message.Exception is not GatewayReconnectException || message.Exception is not TaskCanceledException)
         {
             consoleColor = ConsoleColor.DarkRed;
             FormatColorWrite(message.Exception.GetType().FullName, consoleColor);
