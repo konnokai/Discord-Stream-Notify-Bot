17a56de740b9d5b685dde1a2870b42b25927c7f4
孤之界 <f20001012@Gmail.com>
2025-04-09 11:20:16 +0800

    fix: 嘗試修復 ChangeGuildBannerAsync 的 db 存取問題

--- Discord Stream Notify Bot/SharedService/Youtube/ChangeGuildBanner.cs
+++ Discord Stream Notify Bot/SharedService/Youtube/ChangeGuildBanner.cs
@@ -7,63 +7,81 @@
 #if DEBUG || DEBUG_DONTREGISTERCOMMAND
             return;
 #endif
+            List<DataBase.Table.BannerChange> list;
 
             using (var db = _dbService.GetDbContext())
             {
-                foreach (var item in db.BannerChange.Where(x => x.ChannelId == channelId))
+                list = _dbService.GetDbContext().BannerChange.AsNoTracking()
+                    .Where(x => x.ChannelId == channelId)
+                    .ToList();
+            }
+
+            if (list.Count == 0) return;
+
+            foreach (var item in list)
+            {
+                try
                 {
-                    try
+                    var guild = _client.GetGuild(item.GuildId);
+                    if (guild == null)
                     {
-                        var guild = _client.GetGuild(item.GuildId);
-                        if (guild.PremiumTier < PremiumTier.Tier2) continue;
-
-                        if (videoId != item.LastChangeStreamId)
+                        Log.Warn($"Guild not found: {item.GuildId} / {channelId} / {videoId}");
+                        using (var db = _dbService.GetDbContext())
                         {
-                            MemoryStream memStream;
-                            try
-                            {
-                                memStream = new MemoryStream(await _httpClientFactory.CreateClient("").GetByteArrayAsync($"https://i.ytimg.com/vi/{videoId}/maxresdefault.jpg"));
-                                if (memStream.Length < 2048) memStream = null;
-                            }
-                            catch (Exception ex)
-                            {
-                                Log.Error($"DownloadGuildBanner - {item.GuildId}\n" +
-                                    $"{channelId} / {videoId}\n" +
-                                    $"{ex.Message}\n" +
-                                    $"{ex.StackTrace}");
-                                continue;
-                            }
-
-                            try
-                            {
-                                if (memStream != null)
-                                {
-                                    Image image = new Image(memStream);
-                                    await guild.ModifyAsync((func) => func.Banner = image);
-                                }
-                                item.LastChangeStreamId = videoId;
-                                db.BannerChange.Update(item);
-                                db.SaveChanges();
-
-                                Log.Info("ChangeGuildBanner" + (memStream == null ? "(Without Change)" : "") + $": {item.GuildId} / {videoId}");
-                            }
-                            catch (Exception ex)
-                            {
-                                Log.Error($"ChangeGuildBanner - {item.GuildId}\n" +
-                                    $"{channelId} / {videoId}" +
-                                    $"{ex.Message}\n" +
-                                    $"{ex.StackTrace}");
-                                continue;
-                            }
+                            db.BannerChange.Remove(item);
+                            await db.SaveChangesAsync();
                         }
-                    }
-                    catch (Exception ex)
-                    {
-                        Log.Error($"ChangeGuildBanner - {item.GuildId}\n" +
-                            $"{ex.Message}\n" +
-                            $"{ex.StackTrace}");
                         continue;
                     }
+
+                    if (guild.PremiumTier < PremiumTier.Tier2) continue;
+
+                    if (videoId != item.LastChangeStreamId)
+                    {
+                        MemoryStream memStream;
+                        try
+                        {
+                            memStream = new MemoryStream(await _httpClientFactory.CreateClient("").GetByteArrayAsync($"https://i.ytimg.com/vi/{videoId}/maxresdefault.jpg"));
+                            if (memStream.Length < 2048) memStream = null;
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error($"DownloadGuildBanner - {item.GuildId}\n" +
+                                $"{channelId} / {videoId}\n" +
+                                $"{ex.Message}\n" +
+                                $"{ex.StackTrace}");
+                            continue;
+                        }
+
+                        try
+                        {
+                            if (memStream != null)
+                            {
+                                Image image = new Image(memStream);
+                                await guild.ModifyAsync((func) => func.Banner = image);
+                            }
+                            
+                            item.LastChangeStreamId = videoId;
+
+                            using (var db = _dbService.GetDbContext())
+                            {
+                                db.BannerChange.Update(item);
+                                await db.SaveChangesAsync();
+                            }
+
+                            Log.Info("ChangeGuildBanner" + (memStream == null ? "(Without Change)" : "") + $": {item.GuildId} / {videoId}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex.Demystify(), $"ChangeGuildBanner - {item.GuildId}: {channelId} / {videoId}");
+                            continue;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.Demystify(), $"ChangeGuildBanner - {item.GuildId}");
+                    continue;
                 }
             }
         }
